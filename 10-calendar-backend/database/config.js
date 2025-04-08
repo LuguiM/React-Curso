84ba31dc7dafdb3b7010aff90962c6bc21b188ca
LuigiM <jlpadilla@innovacion.gob.sv>
2025-04-08 15:19:50 -0600

    Configuracion inncesaria de mongoose

--- 10-calendar-backend/database/config.js
+++ 10-calendar-backend/database/config.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.DB_URL), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // useCreateIndex: true,
         };
 
         console.log('DB online');
